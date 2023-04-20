8eefab748e90efb7cd2030bf0524caf7d17530ec
lomik31 <lomik31@codead.dev>
2023-04-21 01:52:27 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+class Program:
+    def main():
+        with open("./key.txt", "r") as f:
+            key = f.read()
+        pass
+
+if __name__ == "__main__": Program.main()
---
+++ vk.py
@@ -0,0 +1,5 @@
+class VK:
+    def __init__(self, TOKEN) -> None:
+        self.token = TOKEN
+    def getPhotos(self, userId):
+        pass
