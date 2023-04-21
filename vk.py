509e16cf0d5d6ada919a5465390001d86b15957b
lomik31 <lomik31@codead.dev>
2023-04-21 11:06:56 +0300

    добавлен запрос список фотографий пользователей vk.py

--- vk.py
+++ vk.py
@@ -1,5 +1,18 @@
+import requests
+
 class VK:
     def __init__(self, TOKEN) -> None:
         self.token = TOKEN
-    def getPhotos(self, userId):
-        pass
+    def getPhotos(self, userId: int):
+        headers = {
+            "Authorization": f"Bearer {self.token}"
+        }
+        params = {
+            "owner_id": userId,
+            "album_id": "profile",
+            "extended": 1,
+            "v": "5.131",
+            "rev": 0
+        }
+        resp = requests.get("https://api.vk.com/method/photos.get", headers=headers, params=params).json()
+        if (resp.get("error") or not resp.get("response")): return {"success": False}        
