015eafa2cfd991d05fb378a03e8bca33d2013993
lomik31 <lomik31@codead.dev>
2023-04-21 11:08:14 +0300

    добавлено обращение к VK, отлов ошибок, получение аргументов из командной строки

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+from vk import VK
+import sys
+
 class Program:
     def main():
         with open("./key.txt", "r") as f:
             key = f.read()
-        pass
+        if (len(sys.argv) < 3): return print("Параметры переданы неверно либо не переданы")
+        res = VK(key).getPhotos(sys.argv[1])
+        if (not res.get("success")): return print("Произошла ошибка при выполнении запроса VK")
 
 if __name__ == "__main__": Program.main()
