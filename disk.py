d6e9cc33b3779da15613ae4e74cf44066b528abd
lomik31 <lomik31@codead.dev>
2023-04-29 21:34:05 +0300

    headers was moved to class property

--- disk.py
+++ disk.py
@@ -2,18 +2,18 @@ import requests
 
 class Disk:
     def __init__(self, TOKEN) -> None:
-        self.token = ""
-    def upload(self, fileLink, fileName):
-        pass
-    def createFolder(self, folderName):
-        headers = {
+        self.token = TOKEN
+        self.headers = {
             "Accept": "application/json",
             "Authorization": f"OAuth {self.token}"
         }
+    def upload(self, fileLink, fileName):
+        pass
+    def createFolder(self, folderName):
         args = {
             "path": folderName
         }
-        res = requests.put("https://cloud-api.yandex.net/v1/disk/resources", params=args, headers=headers)
+        res = requests.put("https://cloud-api.yandex.net/v1/disk/resources", params=args, headers=self.headers)
         if (res.status_code != 201):
             print(res.json())
             from time import localtime, strftime, time
