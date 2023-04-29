fb7b82ab521bb98723a9e9980524ea0ce22e6295
lomik31 <lomik31@codead.dev>
2023-04-29 21:12:37 +0300

    added createFolder function

--- disk.py
+++ disk.py
@@ -5,3 +5,16 @@ class Disk:
         self.token = ""
     def upload(self, fileLink, fileName):
         pass
+    def createFolder(self, folderName):
+        headers = {
+            "Accept": "application/json",
+            "Authorization": f"OAuth {self.token}"
+        }
+        args = {
+            "path": folderName
+        }
+        res = requests.put("https://cloud-api.yandex.net/v1/disk/resources", params=args, headers=headers)
+        if (res.status_code != 201):
+            print(res.json())
+            from time import localtime, strftime, time
+            self.createFolder(strftime('%Y-%m-%d-%H\:%M\:%S', localtime(time())) + "-" + folderName)
