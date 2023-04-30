314928cd29f433bebc50fb56537038ec622d3fce
lomik31 <lomik31@codead.dev>
2023-04-30 03:54:31 +0300

    добавлен вызов создания папки на диске и загрузке каждого файла

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from vk import VK
+from disk import Disk
 import sys
 import time
 
@@ -19,5 +20,10 @@ class Program:
             except: return print("Параметры переданы неверно либо не переданы")
         res = VK(key).getPhotos(sys.argv[1], *args)
         if (not res.get("success")): return print("Произошла ошибка при выполнении запроса VK")
+        disk = Disk(sys.argv[2])
+        disk.createFolder(f"vk-photos-{sys.argv[1]}-{time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))}")
+        d = Program.createFilesDict(res.get("data"))
+        for i in d:
+            disk.upload(d[i], i)
 
 if __name__ == "__main__": Program.main()
