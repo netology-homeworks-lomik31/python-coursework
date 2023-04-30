0c509736626acd2ba15a99ceffbf92e2e67b00ef
lomik31 <lomik31@codead.dev>
2023-04-30 09:19:58 +0300

    улучшение логики createFolder

--- disk.py
+++ disk.py
@@ -24,15 +24,16 @@ class Disk:
         print("Успешно.")
     def createFolder(self, folderName):
         print(f"Создаю папку {folderName}: ", end="")
-        self.folderName = folderName
-        args = {"path": folderName}
+        args = {"path": f"/{folderName}"}
         res = requests.put("https://cloud-api.yandex.net/v1/disk/resources", params=args, headers=self.headers)
-        print("Успешно.")
         if (res.status_code != 201):
             print(f"Неудача.\n{res.json()}.\nПопытка 2: ",end="")
             from time import localtime, strftime, time
-            args = {"path": strftime('%Y-%m-%d-%H\:%M\:%S', localtime(time())) + "-" + folderName}
+            folderName = f"{folderName}-{strftime('%Y-%m-%d-%H.%M.%S', localtime(time()))}"
+            args = {"path": folderName}
             res = requests.put("https://cloud-api.yandex.net/v1/disk/resources", params=args, headers=self.headers)
             if (res.status_code != 201):
                 print("Ошибка.")
                 raise Exception(res.json())
+        self.folderName = folderName
+        print("Успешно.")
