088f235074f2e59b7475a0a6f865203ebb07f031
lomik31 <lomik31@codead.dev>
2023-05-13 02:43:28 +0300

    токен яндекс диска и id vk теперь задаются в интерфейсе программы

--- main.py
+++ main.py
@@ -12,24 +12,21 @@ class Program:
     def main():
         parser = argparse.ArgumentParser()
         parser.add_argument("VKT", metavar="VKTokenFile", type=str, nargs=1, help="Путь к файлу с токеном VK")
-        parser.add_argument("VKID", type=str, nargs=1, help="ID пользователя VK для бэкапа")
-        parser.add_argument("YADT", metavar="YaDiskTokenFile", type=str, nargs=1, help="Путь к файлу с токеном ядиска")
+        # parser.add_argument("VKID", type=str, nargs=1, help="ID пользователя VK для бэкапа")
+        # parser.add_argument("YADT", metavar="YaDiskTokenFile", type=str, nargs=1, help="Путь к файлу с токеном ядиска")
         parser.add_argument("-c", "--count", metavar="count", type=int, nargs=1, required=False, default=[5], help="Количество фото для сохранения (default: 5)")
         # parser.add_argument("--google", metavar="googleTokenFile", type=str, nargs=1, required=False, help="Путь к файлу с токеном google drive")
         parser.add_argument("-a", "--album", metavar="albumID", type=str, nargs=1, required=False, default=["profile"], help="ID альбома, из которого будет производиться загрузка фото (доступные варианты: 'wall', 'profile', 'saved', albumId; default: profile)")
         args = parser.parse_args()
+
+        yaKey = input("Введите токен Яндекс.Диска: ")
+        vkId = input("Введите id пользователя VK для бэкапа: ")
 
         try:
             with open(args.VKT[0], "r") as f:
                 vkKey = f.read()
         except:
             print(f"Файл {args.VKT[0]} не найден")
-            return
-        try:
-            with open(args.YADT[0], "r") as f:
-                yaKey = f.read()
-        except:
-            print(f"Файл {args.YADT[0]} не найден")
             return
         # if (args.google):
         #     try:
@@ -38,10 +35,10 @@ class Program:
         #     except:
         #         print(f"Файл {args.google[0]} не найден")
         #         return
-        res = VK(vkKey).getPhotos(args.VKID[0], args.count[0], args.album[0])
+        res = VK(vkKey).getPhotos(vkId, args.count[0], args.album[0])
         if (not res.get("success")): return print("Произошла ошибка при выполнении запроса VK")
         disk = Disk(yaKey)
-        disk.createFolder(f"vk-photos-{args.VKID[0]}-{args.album[0]}")
+        disk.createFolder(f"vk-photos-{vkId}-{args.album[0]}")
         d = Program.createFilesDict(res.get("data"))
         for i in d:
             disk.upload(d[i], i)
